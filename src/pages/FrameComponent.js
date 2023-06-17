d2288f2fa62a3c46bc8f564a76d888d5849fa3d5
Aman Lakra <81770898+AmanlakraHUB@users.noreply.github.com>
2023-06-17 23:49:36 +0530

    Add files via upload

---
+++ src/pages/FrameComponent.js
@@ -0,0 +1,417 @@
+import { TextField } from "@mui/material";
+import "./FrameComponent.css";
+const FrameComponent = () => {
+  return (
+    <div className="rectangle-parent">
+      <div className="frame-child" />
+      <img className="union-icon" alt="" src="/union.svg" />
+      <div className="frame-parent">
+        <div className="vuesaxoutlineprofile-2user-parent">
+          <img
+            className="vuesaxoutlineprofile-2user-icon"
+            alt=""
+            src="/vuesaxoutlineprofile2user.svg"
+          />
+          <div className="share">Share</div>
+        </div>
+        <img className="frame-item" alt="" src="/vector-10.svg" />
+        <img className="frame-inner" alt="" src="/group-613.svg" />
+      </div>
+      <div className="vuesaxoutlinecategory-parent">
+        <img
+          className="vuesaxoutlinecategory-icon"
+          alt=""
+          src="/vuesaxoutlinecategory.svg"
+        />
+        <div className="home">Home</div>
+      </div>
+      <div className="vuesaxlineartask-square-parent">
+        <img
+          className="vuesaxoutlinecategory-icon"
+          alt=""
+          src="/vuesaxlineartasksquare.svg"
+        />
+        <div className="home">Tasks</div>
+      </div>
+      <div className="vuesaxlinearmessage-parent">
+        <img
+          className="vuesaxoutlinecategory-icon"
+          alt=""
+          src="/vuesaxlinearmessage.svg"
+        />
+        <div className="home">Messages</div>
+      </div>
+      <div className="vuesaxlinearprofile-2user-parent">
+        <img
+          className="vuesaxoutlinecategory-icon"
+          alt=""
+          src="/vuesaxlinearprofile2user.svg"
+        />
+        <div className="home">Members</div>
+      </div>
+      <div className="vuesaxlinearsetting-2-parent">
+        <img
+          className="vuesaxoutlinecategory-icon"
+          alt=""
+          src="/vuesaxlinearsetting2.svg"
+        />
+        <div className="home">Settings</div>
+      </div>
+      <img className="vector-icon" alt="" src="/vector-1.svg" />
+      <img className="frame-child1" alt="" src="/vector-7.svg" />
+      <img className="frame-child2" alt="" src="/vector-8.svg" />
+      <div className="my-projects-parent">
+        <b className="my-projects">my projects</b>
+        <img
+          className="vuesaxoutlineprofile-2user-icon"
+          alt=""
+          src="/vuesaxlinearaddsquare.svg"
+        />
+      </div>
+      <div className="mobile-app-parent">
+        <div className="mobile-app">Mobile App</div>
+        <div className="div">. . .</div>
+        <div className="ellipse-div" />
+      </div>
+      <div className="ellipse-parent">
+        <div className="frame-child3" />
+        <div className="home">Website Redesign</div>
+      </div>
+      <div className="ellipse-group">
+        <div className="frame-child4" />
+        <div className="home">Design System</div>
+      </div>
+      <div className="ellipse-container">
+        <div className="frame-child5" />
+        <div className="home">Wireframes</div>
+      </div>
+      <img className="vuesaxbulkcolorfilter-icon" alt="" src="/group-7.svg" />
+      <img className="group-icon" alt="" src="/group-639.svg" />
+      <div className="project-m">Project M.</div>
+      <div className="rectangle-div" />
+      <div className="frame-group">
+        <div className="group-parent">
+          <img className="frame-child6" alt="" src="/group-633.svg" />
+          <div className="vuesaxlinearfolder-2-parent">
+            <img
+              className="vuesaxlinearfolder-2-icon"
+              alt=""
+              src="/vuesaxlinearfolder2.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon"
+              alt=""
+              src="/vuesaxoutlinemessage.svg"
+            />
+            <div className="comments">12 comments</div>
+            <div className="files">0 files</div>
+          </div>
+        </div>
+        <div className="brainstorming">Brainstorming</div>
+        <div className="div1">. . .</div>
+        <div className="brainstorming-brings-team">{`Brainstorming brings team members' diverse experience into play. `}</div>
+        <div className="low-wrapper">
+          <div className="home">Low</div>
+        </div>
+      </div>
+      <img className="frame-child7" alt="" src="/vector-11.svg" />
+      <div className="frame-child8" />
+      <div className="frame-div">
+        <div className="frame-child9" />
+        <div className="home">To Do</div>
+      </div>
+      <div className="frame-child10" />
+      <div className="frame-child11" />
+      <TextField
+        className="frame-textfield"
+        sx={{ width: 417 }}
+        color="primary"
+        variant="outlined"
+        type="text"
+        name="Search"
+        id="Search"
+        label="Search for anything..."
+        placeholder="Placeholder"
+        size="medium"
+        margin="none"
+      />
+      <div className="frame-container">
+        <div className="anima-agrawal-parent">
+          <div className="anima-agrawal">Anima Agrawal</div>
+          <div className="up-india">U.P, India</div>
+        </div>
+        <img
+          className="vuesaxoutlinearrow-down-icon"
+          alt=""
+          src="/vuesaxoutlinearrowdown.svg"
+        />
+      </div>
+      <img className="frame-child12" alt="" src="/group-640.svg" />
+      <div className="frame-parent1">
+        <div className="vuesaxlinearfilter-parent">
+          <img
+            className="vuesaxlinearfilter-icon"
+            alt=""
+            src="/vuesaxlinearfilter.svg"
+          />
+          <img
+            className="vuesaxoutlinearrow-down-icon1"
+            alt=""
+            src="/vuesaxoutlinearrowdown1.svg"
+          />
+          <div className="filter">Filter</div>
+        </div>
+        <div className="vuesaxlinearfilter-parent">
+          <img className="calendar-icon" alt="" src="/calendar.svg" />
+          <img
+            className="vuesaxoutlinearrow-down-icon2"
+            alt=""
+            src="/vuesaxoutlinearrowdown2.svg"
+          />
+          <div className="filter">Today</div>
+        </div>
+      </div>
+      <div className="mobile-app-group">
+        <div className="mobile-app1">Mobile App</div>
+        <img className="frame-child13" alt="" src="/group-643.svg" />
+      </div>
+      <div className="wrapper">
+        <div className="home">4</div>
+      </div>
+      <div className="container">
+        <div className="home">3</div>
+      </div>
+      <div className="frame">
+        <div className="home">2</div>
+      </div>
+      <div className="frame-child14" />
+      <img className="frame-child15" alt="" src="/vector-12.svg" />
+      <div className="ellipse-parent1">
+        <div className="frame-child3" />
+        <div className="home">On Progress</div>
+      </div>
+      <img className="frame-child17" alt="" src="/vector-13.svg" />
+      <div className="ellipse-parent2">
+        <div className="frame-child5" />
+        <div className="home">Done</div>
+      </div>
+      <div className="frame-child19" />
+      <div className="frame-parent2">
+        <div className="group-container">
+          <img className="frame-child6" alt="" src="/group-6331.svg" />
+          <div className="vuesaxlinearfolder-2-parent">
+            <img
+              className="vuesaxlinearfolder-2-icon"
+              alt=""
+              src="/vuesaxlinearfolder21.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon"
+              alt=""
+              src="/vuesaxoutlinemessage1.svg"
+            />
+            <div className="comments">12 comments</div>
+            <div className="files">15 files</div>
+          </div>
+        </div>
+        <div className="moodboard">Design System</div>
+        <div className="div5">. . .</div>
+        <div className="brainstorming-brings-team">{`It just needs to adapt the UI from what you did before `}</div>
+        <div className="completed-wrapper">
+          <div className="home">Completed</div>
+        </div>
+      </div>
+      <div className="unsplashbs-1xgrkih4-parent">
+        <img
+          className="unsplashbs-1xgrkih4-icon"
+          alt=""
+          src="/unsplashbs-1xgrkih4@2x.png"
+        />
+        <img
+          className="unsplashkiqjfzbii9w-icon"
+          alt=""
+          src="/unsplashkiqjfzbii9w@2x.png"
+        />
+        <div className="ellipse-parent3">
+          <img className="ellipse-icon" alt="" src="/ellipse-12@2x.png" />
+          <div className="vuesaxlinearfolder-2-parent">
+            <img
+              className="vuesaxlinearfolder-2-icon2"
+              alt=""
+              src="/vuesaxlinearfolder22.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon"
+              alt=""
+              src="/vuesaxoutlinemessage2.svg"
+            />
+            <div className="comments">9 comments</div>
+            <div className="files2">10 files</div>
+          </div>
+        </div>
+        <div className="moodboard">Moodboard</div>
+        <div className="div5">. . .</div>
+        <div className="low-wrapper">
+          <div className="home">Low</div>
+        </div>
+      </div>
+      <div className="unsplashmicqqgydq6w-parent">
+        <img
+          className="unsplashmicqqgydq6w-icon"
+          alt=""
+          src="/unsplashmicqqgydq6w@2x.png"
+        />
+        <div className="frame-parent3">
+          <div className="vuesaxlinearfolder-2-parent1">
+            <img
+              className="vuesaxlinearfolder-2-icon"
+              alt=""
+              src="/group-6283.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon"
+              alt=""
+              src="/group-6273.svg"
+            />
+            <div className="comments">14 comments</div>
+            <div className="files">15 files</div>
+          </div>
+          <img className="frame-child21" alt="" src="/ellipse-15@2x.png" />
+          <img className="frame-child22" alt="" src="/ellipse-13@2x.png" />
+          <img className="frame-child23" alt="" src="/ellipse-121@2x.png" />
+        </div>
+        <div className="moodboard">{`Onboarding Illustrations `}</div>
+        <div className="div5">. . .</div>
+        <div className="low-wrapper">
+          <div className="home">Low</div>
+        </div>
+      </div>
+      <div className="rectangle-group">
+        <div className="frame-child24" />
+        <img className="mask-group-icon" alt="" src="/mask-group@2x.png" />
+        <div className="frame-parent4">
+          <div className="vuesaxlinearfolder-2-parent1">
+            <img
+              className="vuesaxlinearfolder-2-icon"
+              alt=""
+              src="/group-6284.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon"
+              alt=""
+              src="/group-6274.svg"
+            />
+            <div className="comments">12 comments</div>
+            <div className="files">15 files</div>
+          </div>
+          <img className="frame-child22" alt="" src="/ellipse-13@2x.png" />
+          <img className="frame-child23" alt="" src="/ellipse-1211@2x.png" />
+        </div>
+        <div className="moodboard">Mobile App Design</div>
+        <div className="div5">. . .</div>
+        <div className="completed-wrapper">
+          <div className="home">Completed</div>
+        </div>
+      </div>
+      <div className="frame-parent5">
+        <div className="vuesaxlinearadd-square-parent">
+          <img
+            className="vuesaxoutlinearrow-down-icon"
+            alt=""
+            src="/vuesaxlinearaddsquare1.svg"
+          />
+          <div className="share">Invite</div>
+        </div>
+        <div className="ellipse-parent4">
+          <img className="frame-child27" alt="" src="/ellipse-122@2x.png" />
+          <img className="frame-child28" alt="" src="/ellipse-131@2x.png" />
+          <img className="frame-child29" alt="" src="/ellipse-151@2x.png" />
+          <img className="frame-child30" alt="" src="/ellipse-14@2x.png" />
+          <div className="frame-child31" />
+          <div className="div9">+2</div>
+        </div>
+      </div>
+      <div className="mask-group">
+        <div className="frame-parent6">
+          <div className="group-parent1">
+            <img className="frame-child6" alt="" src="/group-6332.svg" />
+            <div className="vuesaxlinearfolder-2-parent">
+              <img
+                className="vuesaxlinearfolder-2-icon5"
+                alt=""
+                src="/vuesaxlinearfolder23.svg"
+              />
+              <img
+                className="vuesaxoutlinemessage-icon"
+                alt=""
+                src="/vuesaxoutlinemessage3.svg"
+              />
+              <div className="comments">2 comments</div>
+              <div className="files5">0 files</div>
+            </div>
+          </div>
+          <div className="brainstorming">Wireframes</div>
+          <div className="div1">. . .</div>
+          <div className="brainstorming-brings-team">
+            Low fidelity wireframes include the most basic content and visuals.
+          </div>
+          <div className="high-wrapper">
+            <div className="home">High</div>
+          </div>
+        </div>
+      </div>
+      <div className="mask-group1">
+        <div className="mask-group-child" />
+      </div>
+      <div className="frame-child33" />
+      <div className="write-a-message">Write a message</div>
+      <img
+        className="vuesaxbulklamp-on-icon"
+        alt=""
+        src="/vuesaxbulklampon.svg"
+      />
+      <div className="thoughts-time">Thoughts Time</div>
+      <div className="we-dont-have">
+        We don’t have any notice for you, till then you can share your thoughts
+        with your peers.
+      </div>
+      <img
+        className="vuesaxbulkadd-square-icon"
+        alt=""
+        src="/vuesaxbulkaddsquare.svg"
+      />
+      <div className="rectangle-container">
+        <div className="frame-child34" />
+        <div className="frame-parent7">
+          <div className="vuesaxlinearfolder-2-parent4">
+            <img
+              className="vuesaxlinearfolder-2-icon6"
+              alt=""
+              src="/vuesaxlinearfolder24.svg"
+            />
+            <img
+              className="vuesaxoutlinemessage-icon6"
+              alt=""
+              src="/vuesaxoutlinemessage4.svg"
+            />
+            <div className="comments">10 comments</div>
+            <div className="files">3 files</div>
+          </div>
+          <img className="frame-child35" alt="" src="/group-6333.svg" />
+        </div>
+        <div className="research">Research</div>
+        <div className="div11">. . .</div>
+        <div className="user-research-helps">
+          User research helps you to create an optimal product for users.
+        </div>
+        <div className="high-container">
+          <div className="home">High</div>
+        </div>
+      </div>
+      <img className="cursorgrabbed-icon" alt="" src="/cursorgrabbed.svg" />
+      <img className="mask-group-icon1" alt="" src="/mask-group1@2x.png" />
+    </div>
+  );
+};
+
+export default FrameComponent;
